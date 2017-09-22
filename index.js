91f725dba630944931feb1f3258dc86a75d9f911
Starodubenko Konstantin <kostyast93@gmail.com>
2017-09-22 11:55:51 +0300

    Modified

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const  minify = require('html-minifier').minify;
-const app = express();
-
-app.get('/',function (res,req) {
-    const result = minify('firstpage.html',{removeAttributeQuotes: true});
-    res.render('result')
-});
